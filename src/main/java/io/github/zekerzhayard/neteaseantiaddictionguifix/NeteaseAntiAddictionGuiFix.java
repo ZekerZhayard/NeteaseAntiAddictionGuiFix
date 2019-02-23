051e9da5f9763c3c8dc60f8aca15e2c54c846c33
ZekerZhayard <trees1000@qq.com>
2019-02-23 22:17:42 +0800

    update to 1.2

--- src/main/java/io/github/zekerzhayard/neteaseantiaddictionguifix/NeteaseAntiAddictionGuiFix.java
+++ src/main/java/io/github/zekerzhayard/neteaseantiaddictionguifix/NeteaseAntiAddictionGuiFix.java
@@ -1,31 +1,51 @@
 package io.github.zekerzhayard.neteaseantiaddictionguifix;
 
-import java.lang.reflect.Proxy;
 import java.util.Map;
 
 import org.apache.commons.lang3.reflect.FieldUtils;
 
+import com.netease.mc.mod.antiAddiction.AntiAddictionEventHandlerClient;
+
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.ModContainer;
+import net.minecraftforge.fml.common.Optional;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
-@Mod(modid = "neteaseantiaddictionguifix", name = "NeteaseAntiAddictionGuiFix", version = "@version@", dependencies = "after:antimod;", acceptedMinecraftVersions = "[1.8,1.12.2]")
+@Mod(modid = "neteaseantiaddictionguifix", name = "NeteaseAntiAddictionGuiFix", version = "@version@", clientSideOnly = true, dependencies = "after:antimod;", acceptedMinecraftVersions = "[1.8,1.12.2]")
 public class NeteaseAntiAddictionGuiFix {
+	private AntiAddictionEventHandlerClient aaehc;
+	
 	@Mod.EventHandler()
+	@Optional.Method(modid = "antimod")
 	@SuppressWarnings(value = { "unchecked" })
 	public void init(FMLInitializationEvent event) throws IllegalAccessException {
-		((Map<Object, ModContainer>) FieldUtils.readDeclaredField(MinecraftForge.EVENT_BUS, "listenerOwners", true)).keySet().stream().filter(o -> o.getClass().getName().equals("com.netease.mc.mod.antiAddiction.AntiAddictionEventHandlerClient")).forEachOrdered(o -> {
-			MinecraftForge.EVENT_BUS.unregister(o);
-			try {
-				((Map<String, Object>) FieldUtils.readDeclaredField(Proxy.getInvocationHandler(o.getClass().getMethod("onRenderTickEvent", TickEvent.RenderTickEvent.class).getAnnotation(SubscribeEvent.class)), "memberValues", true)).put("priority", EventPriority.LOWEST);
-			} catch (IllegalAccessException | NoSuchMethodException e) {
-				e.printStackTrace();
+		for (Object o : ((Map<Object, ModContainer>) FieldUtils.readDeclaredField(MinecraftForge.EVENT_BUS, "listenerOwners", true)).keySet()) {
+			if (o instanceof AntiAddictionEventHandlerClient) {
+				this.aaehc = (AntiAddictionEventHandlerClient) o;
+				MinecraftForge.EVENT_BUS.unregister(o);
+				MinecraftForge.EVENT_BUS.register(this);
+				break;
 			}
-			MinecraftForge.EVENT_BUS.register(o);
-		});
+		}
+	}
+	
+	@SubscribeEvent()
+	public void onClientChatReceivedEvent(ClientChatReceivedEvent event) {
+		this.aaehc.ChatMessageReceiveEvent(event);
+	}
+	
+	@SubscribeEvent()
+	public void onClientTickEvent(TickEvent.ClientTickEvent event) {
+		this.aaehc.onClientTickEvent(event);
+	}
+	
+	@SubscribeEvent(priority = EventPriority.LOWEST)
+	public void onRenderTickEvent(TickEvent.RenderTickEvent event) {
+		this.aaehc.onRenderTickEvent(event);
 	}
 }
