a8d0d8a11cecda0529f35daeacb73106ae0c69cc
ZekerZhayard <trees1000@qq.com>
2019-05-11 20:22:41 +0800

    Update to 1.4

--- src/main/java/io/github/zekerzhayard/neteaseantiaddictionguifix/NeteaseAntiAddictionGuiFix.java
+++ src/main/java/io/github/zekerzhayard/neteaseantiaddictionguifix/NeteaseAntiAddictionGuiFix.java
@@ -18,34 +18,29 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 @Mod(modid = "neteaseantiaddictionguifix", name = "NeteaseAntiAddictionGuiFix", version = "@version@", clientSideOnly = true, dependencies = "after:antimod;", acceptedMinecraftVersions = "[1.8,1.12.2]", updateJSON = "https://raw.githubusercontent.com/ZekerZhayard/NeteaseAntiAddictionGuiFix/master/update.json")
 public class NeteaseAntiAddictionGuiFix {
-	private Object aaehc;
-	
-	@Mod.EventHandler()
-	@Optional.Method(modid = "antimod")
-	@SuppressWarnings(value = { "unchecked" })
-	public void init(FMLInitializationEvent event) throws IllegalAccessException {
-		for (Object o : ((Map<Object, ModContainer>) FieldUtils.readDeclaredField(MinecraftForge.EVENT_BUS, "listenerOwners", true)).keySet()) {
-			if (o instanceof AntiAddictionEventHandlerClient) {
-				this.aaehc = o;
-				MinecraftForge.EVENT_BUS.unregister(o);
-				MinecraftForge.EVENT_BUS.register(this);
-				break;
-			}
-		}
-	}
-	
-	@SubscribeEvent()
-	public void onClientChatReceivedEvent(ClientChatReceivedEvent event) {
-		((AntiAddictionEventHandlerClient) this.aaehc).ChatMessageReceiveEvent(event);
-	}
-	
-	@SubscribeEvent()
-	public void onClientTickEvent(TickEvent.ClientTickEvent event) {
-		((AntiAddictionEventHandlerClient) this.aaehc).onClientTickEvent(event);
-	}
-	
-	@SubscribeEvent(priority = EventPriority.LOWEST)
-	public void onRenderTickEvent(TickEvent.RenderTickEvent event) {
-		((AntiAddictionEventHandlerClient) this.aaehc).onRenderTickEvent(event);
-	}
+    private AntiAddictionEventHandlerClient aaehc;
+
+    @Mod.EventHandler()
+    @Optional.Method(modid = "antimod")
+    @SuppressWarnings(value = { "unchecked" })
+    public void init(FMLInitializationEvent event) throws IllegalAccessException {
+        for (Object o : ((Map<Object, ModContainer>) FieldUtils.readDeclaredField(MinecraftForge.EVENT_BUS, "listenerOwners", true)).keySet()) {
+            if (o instanceof AntiAddictionEventHandlerClient) {
+                this.aaehc = (AntiAddictionEventHandlerClient) o;
+                MinecraftForge.EVENT_BUS.unregister(o);
+                MinecraftForge.EVENT_BUS.register(this);
+                break;
+            }
+        }
+    }
+
+    @SubscribeEvent()
+    public void onClientChatReceivedEvent(ClientChatReceivedEvent event) {
+        this.aaehc.ChatMessageReceiveEvent(event);
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public void onRenderTickEvent(TickEvent.RenderTickEvent event) throws IllegalAccessException {
+        this.aaehc.onRenderTickEvent(event);
+    }
 }
